016d680e3bec60e7768a5ce21dc34f94a63b2ef1
JianYu <jyu@jyu-3.home>
2018-05-14 00:19:07 +0200

    change help

--- Help.R
+++ Help.R
@@ -2,9 +2,8 @@ tabPanel("Help",
          fluidRow(
            column(4,wellPanel(
              h4("Instructions"),
-             a("Gene", href = "#Gene"), br(),
-             a("miRNA", href = "#miRNA"), br(),
-             a("Network", href = "#Network"), br()
+             a("Sample Label Switch", href = "#Sample Label Switch"), br()
+
             
            )
            ),#column
--- ui_Gene.R
+++ ui_Gene.R
@@ -18,7 +18,7 @@ ui <- tabPanel("Sample Switch",   ###Tab Name
                     fluidRow(column(3, 
                             fluidRow(column(5, tags$textarea(id="Sample1", rows=8, cols=12, "Sample1\nSample2\nSample3\nSample4\nSample5")),
                                      column(2, tags$h4(),tags$h1(),tags$h1(),tags$h1(),tags$h4("=>")),
-                                     column(5, tags$textarea(id="Sample2", rows=8, cols=12, "Sample5\nSample4\nSample3\nSample2\nSample1"))),
+                                     column(5, tags$textarea(id="Sample2", rows=8, cols=12, "Sample5\nSample4\nSample5\nSample2\nSample1"))),
                             fluidRow(column(5, offset=4, submitButton("Plot")))),
                             column(9,  #verbatimTextOutput("sample1"), verbatimTextOutput("sample2"),
                                    plotOutput("ss_plot"))
